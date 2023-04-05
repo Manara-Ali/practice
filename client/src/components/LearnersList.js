9453021604ff3629c298c51a7024bc5489dbd36b
Manara-Ali <manaraali64@gmail.com>
2023-04-05 10:47:23 -0400

    Front end setup

---
+++ client/src/components/LearnersList.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const LearnersList = () => {
+  const [learners, setLearners] = useState([]);
+
+  useEffect(() => {
+    const fetchLearners = async () => {
+      const response = await axios.get("/learners");
+
+      const { data } = response.data;
+
+      setLearners(data.learners);
+    };
+
+    fetchLearners();
+  }, []);
+
+  const learnersList = learners.map((element) => {
+    return (
+      <div key={element._id}>
+        <p>{element.name}</p>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Per Scholas Learners List</h1>
+      {learnersList}
+    </div>
+  );
+};
+
+export default LearnersList;
