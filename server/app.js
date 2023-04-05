3db08aeb036f746ac310492d464cc4710990a856
Manara-Ali <manaraali64@gmail.com>
2023-04-05 11:43:45 -0400

    adding cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const cors = require("cors");
+
 // IMPORT ROUTER
 const learnerRouter = require("./routes/learnerRouters");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
